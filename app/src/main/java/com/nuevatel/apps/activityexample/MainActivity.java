b8ee1258cdca9158ce5d4c1bbb2e20e385e8b29c
calyr <calyr_software@hotmail.com>
2016-10-18 20:55:32 -0400

    Initial commit

---
+++ app/src/main/java/com/nuevatel/apps/activityexample/MainActivity.java
@@ -0,0 +1,23 @@
+package com.nuevatel.apps.activityexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mostrar(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+
+    }
+
+
+}
